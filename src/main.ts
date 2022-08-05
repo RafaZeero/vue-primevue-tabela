34192de99b343b32c7f0dfd1e5c847ee4eb1e067
Rafael Lima <rafaa99@gmail.com>
2022-08-05 14:03:50 -0300

    update proj

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+// import do prime vue 
+import PrimeVue from 'primevue/config';
+
+// imports do css do primevue
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css';
+
+// imports dos componentes do prime vue
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import InputText from 'primevue/inputtext';
+
+//.component('nome do componente', nome do import do componente)
+createApp(App)
+  .use(router, PrimeVue)
+  .component('DataTable', DataTable)
+  .component('Column', Column)
+  .component('InputText', InputText)
+  .mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>hello</h1>
+      <DataTable :value="products" editMode="row" dataKey="id" v-model:editingRows="editingRows" @row-edit-save="onRowEditSave" responsiveLayout="scroll">
+          <Column field="code" header="Code" style="width:20%">
+              <template #editor="{ data, field }"> products.data.[code]
+                  <InputText v-model="data[field]" autofocus />
+              </template>
+          </Column>
+          <Column field="name" header="Name" style="width:20%">
+              <template #editor="{ data, field }">products.data.[name]
+                  <InputText v-model="data[field]" />
+              </template>
+          </Column>
+          <Column field="price" header="Price" style="width:20%">
+              <template #editor="{ data, field }"> products.data.[price]
+                  <InputText v-model="data[field]" />
+              </template>
+          </Column>
+          <Column :rowEditor="true" style="width:10%; min-width:8rem" bodyStyle="text-align:center"></Column>
+      </DataTable>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {data} from '../data/data'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     HelloWorld,
   },
+  data(){
+    return {
+      editingRows: [],
+      products: null,
+    }
+  },created() {
+        this.columns = [
+            {field: 'code', header: 'Code'},
+            {field: 'name', header: 'Name'},
+            {field: 'quantity', header: 'Quantity'},
+            {field: 'price', header: 'Price'}
+        ];
+    },
+  methods:{
+    // getProducts(){
+    //   this.products = data
+    // },
+    // Use essa função para pegar dados em formato .json e transformá-los em objeto e inseri-los na varíavel desejada
+    getProductsApi(){
+     return fetch('/src/data/db.json').then(resposta=>resposta.json()).then(dados=> dados.data)
+    },
+    onRowEditSave(event) {
+    let { newData, index } = event;
+
+    this.products[index] = newData;
+},
+  },mounted(){
+    // this.getProducts()
+    this.products = this.getProductsApi()
+  }
 });
 </script>
